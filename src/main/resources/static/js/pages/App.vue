9eb09af2151cf2be77b993a0b7e58abdd15b9a35
morkvaivan <morkvaivan@icloud.com>
2020-05-29 15:08:55 +0200

    [feature/adding-webpack-and-split-vue-components]: Added webpack and splitted vue components.

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-if="!profile">Please, authorize with
+            <a href="/login">Google</a>
+        </div>
+        <div v-else>
+            <div>{{profile.name}}&nbsp;<a href="/logout">Log Out</a></div>
+             <messages-list :messages="messages" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import MessagesList from 'components/messages/MessageList.vue';
+
+    export default {
+        components: {
+            MessagesList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
