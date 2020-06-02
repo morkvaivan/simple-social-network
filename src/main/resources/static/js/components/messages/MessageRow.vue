1a0ac91560d09e971fc9a5deada436caddaf3b45
morkvaivan <morkvaivan@icloud.com>
2020-06-02 15:34:42 +0200

    [feature/vuetify]: Added styles.

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
-        <i>({{ message.id }})</i> {{ message.text }}
-        <span style="position: absolute; right: 0;" >
-            <input type="button" value="Edit" @click="edit" />
-            <input type="button" value="X" @click="del" />
-        </span>
-    </div>
+    <v-card class="my-2">
+        <v-card-text>
+            <i>({{ message.id }})</i>
+            {{ message.text }}
+        </v-card-text>
+        <v-card-actions>
+            <v-btn small text rounded value="Edit" @click="edit">Edit</v-btn>
+            <v-btn small icon value="X" @click="del">
+                <v-icon>{{ deleteIcon }}</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
+    import { mdiDelete } from '@mdi/js'
+
     export default {
         props: ['message', 'editMessage', 'deleteMessage', 'messages'],
+        data() {
+            return {
+                deleteIcon: mdiDelete
+            }
+        },
         methods: {
             edit() {
                 this.editMessage(this.message);
