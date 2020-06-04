afaaa378a11121594ca8c777d842c2e6143b112e
morkvaivan <morkvaivan@icloud.com>
2020-06-04 13:30:27 +0200

    [feature/comments-by-jpa-entity-graph]: Added comments feature.

---
+++ src/main/java/example/simplesocialnetwork/service/CommentService.java
@@ -0,0 +1,25 @@
+package example.simplesocialnetwork.service;
+
+import example.simplesocialnetwork.domain.Comment;
+import example.simplesocialnetwork.domain.User;
+import example.simplesocialnetwork.repo.CommentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    private final CommentRepo commentRepo;
+
+    @Autowired
+    public CommentService(CommentRepo commentRepo) {
+        this.commentRepo = commentRepo;
+    }
+
+    public Comment create(Comment comment, User user) {
+        comment.setAuthor(user);
+
+        commentRepo.save(comment);
+
+        return comment;
+    }
+}
