d5508549179c7ba1bb666d7673e635ad9fa8155c
morkvaivan <morkvaivan@icloud.com>
2020-06-10 18:46:06 +0200

    [feature/subscription-confirmation-on-front-end]: Added the feature.

--- src/main/java/example/simplesocialnetwork/controller/ProfileController.java
+++ src/main/java/example/simplesocialnetwork/controller/ProfileController.java
@@ -2,11 +2,14 @@ package example.simplesocialnetwork.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import example.simplesocialnetwork.domain.User;
+import example.simplesocialnetwork.domain.UserSubscription;
 import example.simplesocialnetwork.domain.Views;
 import example.simplesocialnetwork.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("profile")
@@ -35,5 +38,22 @@ public class ProfileController {
         } else {
             return profileService.changeSubscription(channel, subscriber);
         }
+    }
+
+    @GetMapping("get-subscribers/{channelId}")
+    @JsonView(Views.IdName.class)
+    public List<UserSubscription> subscribers(
+            @PathVariable("channelId") User channel
+    ) {
+        return profileService.getSubscribers(channel);
+    }
+
+    @PostMapping("change-status/{subscriberId}")
+    @JsonView(Views.IdName.class)
+    public UserSubscription changeSubscriptionStatus(
+            @AuthenticationPrincipal User channel,
+            @PathVariable("subscriberId") User subscriber
+    ) {
+        return profileService.changeSubscriptionStatus(channel, subscriber);
     }
 }
