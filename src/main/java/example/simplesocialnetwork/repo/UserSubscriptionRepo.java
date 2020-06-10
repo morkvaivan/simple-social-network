fc2e0589c8c1e1c866924b19dcb3c420347956d1
morkvaivan <morkvaivan@icloud.com>
2020-06-10 16:43:23 +0200

    [feature/subscriptions-with-confirmation]: Added the feature.

---
+++ src/main/java/example/simplesocialnetwork/repo/UserSubscriptionRepo.java
@@ -0,0 +1,12 @@
+package example.simplesocialnetwork.repo;
+
+import example.simplesocialnetwork.domain.User;
+import example.simplesocialnetwork.domain.UserSubscription;
+import example.simplesocialnetwork.domain.UserSubscriptionId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserSubscriptionRepo extends JpaRepository<UserSubscription, UserSubscriptionId> {
+    List<UserSubscription> findBySubscriber(User user);
+}
--- src/main/java/example/simplesocialnetwork/service/ProfileService.java
+++ src/main/java/example/simplesocialnetwork/service/ProfileService.java
@@ -1,11 +1,14 @@
 package example.simplesocialnetwork.service;
 
 import example.simplesocialnetwork.domain.User;
+import example.simplesocialnetwork.domain.UserSubscription;
 import example.simplesocialnetwork.repo.UserDetailsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfileService {
@@ -17,12 +20,19 @@ public class ProfileService {
     }
 
     public User changeSubscription(User channel, User subscriber) {
-        Set<User> subscribers = channel.getSubscribers();
+        List<UserSubscription> subscriptions = channel.getSubscribers()
+                .stream()
+                .filter(subscription ->
+                        subscription.getSubscriber().equals(subscriber)
+                )
+                .collect(Collectors.toList());
 
-        if (subscribers.contains(subscriber)) {
-            subscribers.remove(subscriber);
+        if (subscriptions.isEmpty()) {
+            UserSubscription subscription = new UserSubscription(channel, subscriber);
+
+            channel.getSubscribers().add(subscription);
         } else {
-            subscribers.add(subscriber);
+            channel.getSubscribers().removeAll(subscriptions);
         }
 
         return userDetailsRepo.save(channel);
